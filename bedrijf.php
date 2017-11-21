db61c4324536673913578aed0d450916f6359e38
Accep <petercserei@gmail.com>
2017-11-21 20:06:54 +0100

    On branch master

---
+++ bedrijf.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" href="stylesheet.css" type="text/css">
+        <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+    </head>
+    <body>
+        <div class="picture_header">
+            
+        </div>
+        <nav class="shadow">
+            <ul>
+                <li><a href="home.php"><i class="fa fa-bars fa-2x" aria-hidden="true"></i></i><p>home</p></a></li>
+                <li><a class="current_page" href="bedrijf.php"><i class="fa fa-building-o fa-2x" aria-hidden="true"></i><p>bedrijf</p></a></li>
+                <li><a href="portfolio.php"><i class="fa fa-folder-open-o fa-2x" aria-hidden="true"></i><p>portfolio</p></a></li>
+                <li><a href="contact.php"><i class="fa fa-phone fa-2x" aria-hidden="true"></i></i></i><p>contact</p></a></li>
+            </ul>
+        </nav>
+        <div class="container">
+            <div class="grid_bedrijf">
+                
+                
+            </div>
+        </div>
+        <footer>
+            <div class="grid_footer">
+                <div class="f1">
+<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p></div>
+            </div>
+        </footer>
+    </body>
+</html>
