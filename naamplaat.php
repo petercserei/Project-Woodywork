f2d5dd335ab0ffb8331930d77140a9b57ec2ef81
JulianLambers <groteoren@gmail.com>
2017-11-28 15:46:54 +0100

    naamplaat.php

--- naamplaat.php
+++ naamplaat.php
@@ -1,35 +1,143 @@
-<!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>naamplaat_page</title>
+        <title></title>
         <link rel="stylesheet" href="stylesheet.css" type="text/css">
         <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
     </head>
     <body>
-        <div class="login_nav">
-            <div class="center_nav">
-            <li><a href="inloggen.php"><p>inloggen</p></a></li>
-            <li><p>|</p></li>
-            <li><a href="winkelwagen.php"><p>winkelwagen (0)</p></a></li>
-            </div>
-        </div>
         <div class="picture_header">
             
         </div>
         <nav class="shadow">
             <ul>
-                <li><a href="home.php"><i class="fa fa-bars fa-2x" aria-hidden="true"></i></i><p>home</p></a></li>
+                <li><a class="current_page" href="home.php"><i class="fa fa-bars fa-2x" aria-hidden="true"></i></i><p>home</p></a></li>
                 <li><a href="bedrijf.php"><i class="fa fa-building-o fa-2x" aria-hidden="true"></i><p>bedrijf</p></a></li>
                 <li><a href="portfolio.php"><i class="fa fa-folder-open-o fa-2x" aria-hidden="true"></i><p>portfolio</p></a></li>
-                <li><a class="current_page" href="naamplaat.php"><i class="fa fa-picture-o fa-2x" aria-hidden="true"></i><p>naamplaat</p></a></li>
                 <li><a href="contact.php"><i class="fa fa-phone fa-2x" aria-hidden="true"></i></i></i><p>contact</p></a></li>
             </ul>
         </nav>
         <div class="container">
-            <div class="grid_bedrijf">
-                
-                
+            <div class="grid_home">
+<!--                <div class="h1">
+                </div>-->
+<div class="h2"><h2>Naamplaat maker</h2>
+    <p> 
+    <?php
+       function tekst_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+   }
+    
+   $tekst = $hoogte = $hout = $tkleur = $pkleur = "";
+   $teksterr = $hoogteerr = $houterr = $tkleurerr = $pkleurerr = "";
+   
+   if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if(empty($_POST["tekst"])) {
+            $teksterr = "Tekst is required";       
+   }else{
+       $tekst = tekst_input($_POST["tekst"]);
+       if(!preg_match("/^[a-zA-Z ]*$/",$tekst)){
+           $teksterr = "Alleen letters en spaties toegestaan";
+       }
+   }    
+   }
+   
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if(empty($_POST["hoogte"])) {
+            $hoogteerr = "Hoogte is required";       
+   }
+    }
+    
+       if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if(empty($_POST["hout"])) {
+            $houterr = "Hout is required";       
+   }
+    }
+   
+           if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if(empty($_POST["tkleur"])) {
+            $tkleurerr = "Tekst kleur is required";       
+   }
+    }
+    
+           if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if(empty($_POST["pkleur"])) {
+            $pkleurerr = "Plaat kleur is required";       
+   }
+    }
+
+   
+    
+    
+    
+    ?>   
+    <div class="container">
+        <p><span class="error">* required field.</span></p>
+        <form class="container" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+         <span class="error">* <?php echo $teksterr;?></span>   
+         Tekst: <input type="text" name="tekst" value="<?php echo $tekst;?>"><br>
+         <?php echo $tekst;?>
+         <span class="error">* <?php echo $hoogteerr;?></span>
+         Afmeting:<br>
+         hoogte:
+         <input type="hoogte" name="hoogte" value="<?php echo $hoogte;?>"><br>
+         breedtje
+         lengte
+         
+         
+         
+         <span class="error">* <?php echo $houterr;?></span>
+         Hout: <input list="browsers" name="Hout">
+                      <datalist id="browsers">
+                        <option value="Hout 1">
+                        <option value="Hout 2">
+                        <option value="Hout 3">
+                        <option value="Hout 4">
+                        <option value="Hout 5">
+                      </datalist><br>
+         
+         <span class="error">* <?php echo $tkleurerr;?></span>
+         Tekst kleur:  <input list="browsers" name="tkleur">
+                      <datalist id="browsers">
+                        <option value="Rood">
+                        <option value="Groen">
+                        <option value="Blauw">
+                        <option value="Geel">
+                        <option value="Zwart">
+                      </datalist><br>
+         
+         <span class="error">* <?php echo $pkleurerr;?></span>
+         Plaat kleur: <input list="browsers" name="pkleur">
+                      <datalist id="browsers">
+                        <option value="Rood">
+                        <option value="Groen">
+                        <option value="Blauw">
+                        <option value="Geel">
+                        <option value="Zwart">
+                      </datalist><br>
+         
+          
+         
+          
+          <input type="submit" name="submit" value="Submit">
+        </form> 
+    </div>
+      <?php
+echo "<h2>Your Input:</h2>";
+echo $tekst;
+echo "<br>";
+echo $afmeting;
+echo "<br>";
+echo $materiaal;
+echo "<br>";
+echo $tkleur;
+echo "<br>";
+echo $pkleur;
+?>
+    </p></div>
             </div>
         </div>
         <footer>
