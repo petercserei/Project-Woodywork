72effe17a962cffbff0943059a2fb79cef719bee
ridewo <ridewo@outlook.com>
2017-12-07 11:01:31 +0100

    test

--- home.php
+++ home.php
@@ -62,7 +62,7 @@ $nieuws->execute();
                                     );
                             ?>
                         </div>
-                        <div >
+                        <div>
                             <?php
                             $row = $nieuws->fetch();
                             print(
