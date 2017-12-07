df68b5a58b43b03f614e7f36ab34106484455e93
wesleuyh <wesley_dekking@hotmail.nl>
2017-12-07 14:19:56 +0100

    test

--- bedrijf.php
+++ bedrijf.php
@@ -9,13 +9,13 @@
     <body>
         <div class="login_nav">
             <div class="center_nav">
-            <li><a href="inloggen.php"><p>inloggen</p></a></li>
-            <li><p>|</p></li>
-            <li><a href="winkelwagen.php"><p>winkelwagen (0)</p></a></li>
+                <li><a href="inloggen.php"><p>inloggen</p></a></li>
+                <li><p>|</p></li>
+                <li><a href="winkelwagen.php"><p>winkelwagen (0)</p></a></li>
             </div>
         </div>
         <div class="picture_header">
-            
+
         </div>
         <nav class="shadow">
             <ul>
@@ -28,14 +28,18 @@
         </nav>
         <div class="container">
             <div class="grid_bedrijf">
-                
-                
+                <div class="b1">Box 1</div>
+                <div class="b2">Box 2</div>
+                <div class="b3">Box 3</div>
+                <div class="b4">Box 4</div>
+
+
             </div>
         </div>
         <footer>
             <div class="grid_footer">
                 <div class="f1">
-<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p></div>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p></div>
             </div>
         </footer>
     </body>
